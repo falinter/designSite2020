47f94276f93cf0b42fa4d94dff9f424953ee1e17
John Holst <johnlholst@gmail.com>
2020-08-07 11:35:18 -0500

    files

---
+++ inc/btm.php
@@ -0,0 +1,40 @@
+	<div id="footer-container">
+		<footer class="wrapper1">
+			<p>&copy; Copyright 2011. Design by John Holst</p>
+		</footer>
+		<footer class="wrapper2">
+		<!-- Place this tag where you want the +1 button to render -->
+
+			<p>You can find me on <a href="http://twitter.com/#!/falinter">Twitter</a>, <a href="http://www.facebook.com/falinter">Facebook</a> & <a href="http://www.linkedin.com/in/johnlholst">LinkedIn.</a> | <g:plusone size="small" annotation="none"></g:plusone>
+
+<!-- Place this render call where appropriate -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script></p>
+		</footer>		
+	</div>
+	<div id="preload">
+   <img src="img/logo2.png" width="1" height="1" alt="John Holst. User Interface Designer. Web Manager." />
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+<script src="js/script.js"></script>
+<script>
+	var _gaq=[['_setAccount','UA-13160091-1'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
+<!--[if lt IE 7 ]>
+	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+	<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+<![endif]-->
+
+</body>
+</html>
---
+++ inc/top.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>John Holst - Web Project Manager and UI Designer</title>
+	<meta name="description" content="John Holst is a Web Project Manager and UI designer in the greater Chicago area.">
+	<meta name="author" content="John Holst">
+	<meta name="google-site-verification" content="4a7fyEVxWYaAht9HqLm3D-Fg9F8NGukhso1mkqxsAuw" />
+<!-- Update your html tag to include the itemscope and itemtype attributes -->
+<html itemscope itemtype="http://schema.org/Person">
+
+<!-- Add the following three tags inside head -->
+<meta itemprop="name" content="John Holst - Portfolio">
+<meta itemprop="description" content="Web Project Manager &amp; UI Designer in the greater Chicago area">
+
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<link rel="stylesheet" href="style.css">
+
+	<script src="js/libs/modernizr-2.0.min.js"></script>
+	<script src="js/libs/respond.min.js"></script>
+	<script type="text/javascript">
+    window._idl = {};
+    _idl.variant = "banner";
+    (function() {
+        var idl = document.createElement('script');
+        idl.type = 'text/javascript';
+        idl.async = true;
+        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+        document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+</script>
+</head>
+<body>
+<a id="tag" href="http://internetdefenseleague.org"><img src="http://internetdefenseleague.org/images/badges/final/banner_left.png" alt="Member of The Internet Defense League" /></a>
